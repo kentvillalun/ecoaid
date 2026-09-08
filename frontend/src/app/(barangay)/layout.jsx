37c4657236a90069a315928b493a81f917ca21e8
KentDoCode <villalunkent03@gmail.com>
2026-09-08 18:29:22 +0800

    added applewebapp ot teh metedata abrangay layout file

--- frontend/src/app/(barangay)/layout.jsx
+++ frontend/src/app/(barangay)/layout.jsx
@@ -1,7 +1,14 @@
+import { time } from "motion";
 import BarangayLayoutClient from "./BarangayLayoutClient";
+import { title } from "motion/react-client";
 
 export const metadata = {
-  manifest: "/barangay-manifest.json"
+  manifest: "/barangay-manifest.json",
+  title: "EcoAid",
+  appleWebApp: {
+    title: "EcoAid"
+  }
+
 }
 
 export default function BarangayLayout({ children }) {
